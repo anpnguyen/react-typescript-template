ee4d802e59e2cfb1d59a0aaa181ab26ca6bcbbda
washington-shoji <shoji.washington@gmail.com>
2022-01-11 22:50:08 +1100

    added the initial main test page to the main router

---
+++ src/pages/MainTestPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainTestPage = () => {
+	return <div></div>;
+};
+
+export default MainTestPage;
--- src/routes/main-routes.tsx
+++ src/routes/main-routes.tsx
@@ -1,5 +1,10 @@
 import {Route, Switch} from 'react-router-dom';
+import MainTestPage from '../pages/MainTestPage';
 
 export const MainRoutes = (): JSX.Element => {
-	return <Switch></Switch>;
+	return (
+		<Switch>
+			<Route exact path='/' component={MainTestPage} />
+		</Switch>
+	);
 };
