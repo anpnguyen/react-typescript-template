a840993693545215ff1b93e2de24ff361c8b5ccc
washington-shoji <shoji.washington@gmail.com>
2022-01-19 16:07:37 +1100

    feat(order pages components): Added the initial table component to the order pages

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,9 +1,96 @@
 import React from 'react';
+import { Table } from '../components/table/Table';
+
+const latestOrders = {
+    header: ['order id', 'name', 'phone', 'quantity'],
+    body: [
+        {
+            id: '#OD1111',
+            name: 'Akita Inu',
+            phone: 1111,
+            quantity: 61,
+        },
+        {
+            id: '#OD2222',
+            name: 'Kai Ken',
+            phone: 2211,
+            quantity: 43,
+        },
+        {
+            id: '#OD3333',
+            name: 'Hokkaido Inu',
+            phone: 3311,
+            quantity: 52,
+        },
+        {
+            id: '#OD4444',
+            name: 'Kishu Ken',
+            phone: 4411,
+            quantity: 55,
+        },
+        {
+            id: '#OD5555',
+            name: 'Shiba Inu',
+            phone: 5511,
+            quantity: 45,
+        },
+        {
+            id: '#OD6666',
+            name: 'Shikoku Ken',
+            phone: 6611,
+            quantity: 53,
+        },
+    ],
+};
+
+interface RenderOrderHeaderProps {
+    header: string[];
+}
+
+interface RenderOrderBodyProps {
+    id: string;
+    name: string;
+    phone: number;
+    quantity: number;
+}
+
+const renderOrderHead = (item: RenderOrderHeaderProps, index: number) => (
+    <th key={index}>{item}</th>
+);
+
+const renderOrderBody = (item: RenderOrderBodyProps, index: number) => (
+    <tr key={index}>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>{item.phone}</td>
+        <td>{item.quantity}</td>
+    </tr>
+);
 
 const Orders = () => {
     return (
-        <div style={{ height: '100vh', backgroundColor: 'lightpink' }}>
-            <h1>Main Test Page</h1>
+        <div>
+            <h2 className="page-header">orders</h2>
+            <div className="row">
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card__body">
+                            <Table
+                                headData={latestOrders.header}
+                                renderHead={(
+                                    item: RenderOrderHeaderProps,
+                                    index: number
+                                ) => renderOrderHead(item, index)}
+                                bodyData={latestOrders.body}
+                                renderBody={(
+                                    item: RenderOrderBodyProps,
+                                    index: number
+                                ) => renderOrderBody(item, index)}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
