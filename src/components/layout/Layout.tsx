5b5c184f553c69e68b6a02945fb93cb8bf819156
washington-shoji <shoji.washington@gmail.com>
2022-01-19 16:09:06 +1100

    refactor(minor): Minor refactoring

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import { RouteLocation } from '../../interfaces/route_location.interface';
+import { selectSidebarMenuState } from '../../redux/sidebar-menu/sidebar-menu-slice';
 import { MainRoutes } from '../../routes/main-routes';
+import { useAppSelector } from '../../utils/hooks/redux-toolkit-hooks';
 import { Sidebar } from '../sidebar/Sidebar';
 
 import './Layout.css';
 import { TopNavBar } from './top-navigation-bar/TopNavbar';
 
 export const Layout = (): JSX.Element => {
+    const isOpen = useAppSelector(selectSidebarMenuState);
     return (
         <BrowserRouter>
             <Route
@@ -14,7 +17,7 @@ export const Layout = (): JSX.Element => {
                     <div className="layout">
                         <TopNavBar />
                         <Sidebar {...props} />
-                        <div className="layout__content">
+                        <div className={'layout__content ' + !isOpen}>
                             <main className="layout__content-main">
                                 <MainRoutes />
                             </main>
