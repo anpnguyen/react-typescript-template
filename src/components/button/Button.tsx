9fc40e73b543ed7d2b152dc97239080141cdc018
washington-shoji <shoji.washington@gmail.com>
2022-01-20 21:19:05 +1100

    feat(component): Added a button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,18 @@
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Button.css';
+
+interface IButtonProps {
+    handleCloseOrderForm?(): void;
+}
+
+export const Button = (props: IButtonProps): JSX.Element => {
+    return (
+        <div className="button__wrapper">
+            <button className="button" onClick={props.handleCloseOrderForm}>
+                <FontAwesomeIcon icon={faCartPlus} className="button__icon" />
+                <p>Add order</p>
+            </button>
+        </div>
+    );
+};
