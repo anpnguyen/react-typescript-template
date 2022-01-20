ed93d201b902bbf5dc9f9bf07d87137978189011
washington-shoji <shoji.washington@gmail.com>
2022-01-20 21:25:53 +1100

    refactor(component): Refactored the order and dashboard pages

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Modal } from '../components/modal/Modal';
 
 const Dashboard = () => {
     return (
         <div style={{ height: '100vh', backgroundColor: 'lightblue' }}>
             <h1>Main Test Page</h1>
+            <Modal />
         </div>
     );
 };
--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '../components/button/Button';
 import { SpinnerLoader } from '../components/loader/SpinnerLoader';
+import { Modal } from '../components/modal/Modal';
 import { Table } from '../components/table/Table';
 import { latestOrders } from '../data/dummy-orders';
 import { useGetOrders } from '../utils/hooks/query/use-queries';
@@ -23,10 +25,26 @@ const renderOrderBody = (item: IRenderOrderBodyProps, index: number) => (
 
 const Orders = () => {
     const order = useGetOrders();
+    //const order: any = [];
+
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+    function handleOpenOrderForm(): void {
+        setModalOpen(true);
+    }
+    function handleCloseOrderForm(): void {
+        setModalOpen(false);
+    }
 
     return (
         <div>
-            <h2 className="page-header">orders</h2>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <h2 className="page-header">orders</h2>
+                <div onClick={handleOpenOrderForm}>
+                    <Button />
+                </div>
+            </div>
+
             <div className="row">
                 <div className="col-12">
                     <div className="card">
@@ -34,18 +52,27 @@ const Orders = () => {
                             <SpinnerLoader />
                         ) : (
                             <div className="card__body">
-                                <Table
-                                    headData={latestOrders.header}
-                                    renderHead={(
-                                        item: IRenderOrderHeaderProps,
-                                        index: number
-                                    ) => renderOrderHead(item, index)}
-                                    bodyData={order}
-                                    renderBody={(
-                                        item: IRenderOrderBodyProps,
-                                        index: number
-                                    ) => renderOrderBody(item, index)}
-                                />
+                                {modalOpen ? (
+                                    <Modal
+                                        modalOpen={modalOpen}
+                                        handleCloseOrderForm={
+                                            handleCloseOrderForm
+                                        }
+                                    />
+                                ) : (
+                                    <Table
+                                        headData={latestOrders.header}
+                                        renderHead={(
+                                            item: IRenderOrderHeaderProps,
+                                            index: number
+                                        ) => renderOrderHead(item, index)}
+                                        bodyData={order}
+                                        renderBody={(
+                                            item: IRenderOrderBodyProps,
+                                            index: number
+                                        ) => renderOrderBody(item, index)}
+                                    />
+                                )}
                             </div>
                         )}
                     </div>
