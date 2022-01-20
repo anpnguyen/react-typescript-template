1800c96923f5355d2a92335e8e9fcc5952152586
washington-shoji <shoji.washington@gmail.com>
2022-01-21 10:07:51 +1100

    refactor(styling and state): Refactored the modal styling and the state management to redux

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
+import { selectOrderModalState } from '../../redux/order-modal/order-modal';
+import { useAppSelector } from '../../utils/hooks/redux/redux-toolkit-hooks';
 import { IOrderFormData, OrderForm } from '../order-form/OrderForm';
 import './Modal.css';
 
 export const Modal = (props: any) => {
     const [orderFormData, setOrderFormData] = useState<IOrderFormData[]>([]);
-    const modalOpen = props.modalOpen;
+    const modalOpen: boolean = useAppSelector(selectOrderModalState);
+    const modalClass = modalOpen ? '-active' : '-notactive';
 
     function addOrderFormData(orderFormData: IOrderFormData): void {
         setOrderFormData([orderFormData]);
     }
 
     return (
-        <div className={'modal__wrapper' + modalOpen}>
+        <div className={'modal__wrapper' + modalClass}>
             <div className="modal__inner">
                 <OrderForm
                     handleCloseOrderForm={props.handleCloseOrderForm}
