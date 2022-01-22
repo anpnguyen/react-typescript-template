5619d85b140dd7bcd70c7f308d5804753fbbea17
washington-shoji <shoji.washington@gmail.com>
2022-01-22 21:18:14 +1100

    refactor(components): Refactored the modal component and it's styling properties

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -13,10 +13,8 @@ export const Modal = (props: any) => {
         setOrderFormData([orderFormData]);
     }
 
-    console.log('orderFormData', orderFormData);
-
     return (
-        <div className={'modal__wrapper' + modalClass}>
+        <div className={'modal__wrapper'}>
             <div className="modal__inner">
                 <OrderForm
                     handleCloseOrderForm={props.handleCloseOrderForm}
