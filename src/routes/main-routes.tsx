0f519812ad344ce2fd7b6201f11c14d56b4f1f06
washington-shoji <shoji.washington@gmail.com>
2022-01-22 21:18:36 +1100

    refactor(minor): Minor refactoring

--- src/routes/main-routes.tsx
+++ src/routes/main-routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 import Customers from '../pages/Customers';
 import Dashboard from '../pages/Dashboard';
+import OrderFormPage from '../pages/OrderFormPage';
 import Orders from '../pages/Orders';
 import Products from '../pages/Products';
 
@@ -11,6 +12,7 @@ export const MainRoutes = (): JSX.Element => {
             <Route exact path="/orders" component={Orders} />
             <Route exact path="/customers" component={Customers} />
             <Route exact path="/products" component={Products} />
+            <Route exact path="/order-form" component={OrderFormPage} />
         </Switch>
     );
 };
