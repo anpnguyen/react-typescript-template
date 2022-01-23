2d62bdb86ca5abbda0bb50c756461b74abcf9993
washington-shoji <shoji.washington@gmail.com>
2022-01-23 20:54:09 +1100

    refactor(minor): Refactored the redux store to add the reducers

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,11 +1,19 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import sidebarMenuReducer from '../sidebar-menu/sidebar-menu-slice';
 import orderModalReducer from '../order-modal/order-modal';
+import getOrdersReducer from '../order-api-calls/get-orders-slice';
+import createOrderReducer from '../order-api-calls/create-order-slice';
+import deleteOrderReducer from '../order-api-calls/delete-api-call';
+import updateOrderReducer from '../order-api-calls/update-order-slice';
 
 export const store = configureStore({
     reducer: {
         sidebarMenu: sidebarMenuReducer,
         orderModal: orderModalReducer,
+        getOrders: getOrdersReducer,
+        creteOrder: createOrderReducer,
+        deleteOrder: deleteOrderReducer,
+        updateOrder: updateOrderReducer,
     },
     devTools: true,
 });
