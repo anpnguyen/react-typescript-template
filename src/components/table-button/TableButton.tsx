77fede8973f31d61c7735e094abe76aa87ac03fb
washington-shoji <shoji.washington@gmail.com>
2022-01-23 20:53:13 +1100

    refactor(minor): Refactored the table button component

--- src/components/table-button/TableButton.tsx
+++ src/components/table-button/TableButton.tsx
@@ -4,12 +4,17 @@ import './TableButton.css';
 interface ITableButtonProps {
     onClick?(): void;
     title: string;
+    submitting?: boolean;
 }
 
 export const TableButton = (props: ITableButtonProps): JSX.Element => {
     return (
         <div className={'table-button__wrapper' + ` ${props.title}`}>
-            <button className="table-button" onClick={props.onClick}>
+            <button
+                className="table-button"
+                onClick={props.onClick}
+                disabled={props.submitting ? true : false}
+            >
                 <p>{props.title}</p>
             </button>
         </div>
