69333ee9bd3c74c02efe0424a05fd573d577b433
washington-shoji <shoji.washington@gmail.com>
2022-01-29 21:22:16 +1100

    feat(component): Added a temporary landing component and styling

---
+++ src/components/lading/Landing.tsx
@@ -0,0 +1,26 @@
+import { useAuth0, User } from '@auth0/auth0-react';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { Button } from '../button/Button';
+import './Landing.css';
+
+export default function Landing() {
+    const { loginWithRedirect } = useAuth0<User>();
+    return (
+        <main>
+            <div className="landing_container">
+                <div className="landing_header">
+                    <h1>Hi there welcome to JSD Dev Hub.</h1>
+                    <p>Please login to get started.</p>
+                </div>
+
+                <div className="landing_button">
+                    <Button
+                        title="Login"
+                        icon={faSignInAlt}
+                        onClick={loginWithRedirect}
+                    />
+                </div>
+            </div>
+        </main>
+    );
+}
