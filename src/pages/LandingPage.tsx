b73b3e4ce1a7b5534d35a99757565de280796220
washington-shoji <shoji.washington@gmail.com>
2022-02-01 21:05:49 +1100

    refactor(styling): Refactored the landing page styling

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,17 @@
 import Landing from '../components/lading/Landing';
+import image from '../../src/assets/images/bg_1.jpg';
 
 export default function LandingPage() {
     return (
-        <div>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                backgroundImage: `url(${image})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+            }}
+        >
             <Landing />
         </div>
     );
