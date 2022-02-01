74b21267b05aa9a78a7ab3801c151e3f5aaa1962
washington-shoji <shoji.washington@gmail.com>
2022-02-01 21:07:01 +1100

    feat(component): Added a accordion component

---
+++ src/components/customers/components/accordion/Accordion.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { IAccordionData } from '../../../../pages/Customers';
+import AccordionButton from '../accordion-button/AccordionButton';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import './Accordion.css';
+
+export default function Accordion(data: IAccordionData): JSX.Element {
+    const [isActive, setIsActive] = useState(false);
+
+    const { id, title, info, image, content } = data;
+    return (
+        <div className="accordion">
+            <div
+                className="accordion__header"
+                onClick={() => setIsActive(!isActive)}
+            >
+                <div>
+                    <p>id: {id}</p>
+                </div>
+                <div>
+                    <p>title: {title}</p>
+                </div>
+                <div>
+                    <p>info: {info}</p>
+                </div>
+            </div>
+            {isActive && (
+                <div className="accordion__content">
+                    <div className="accordion__content___image">
+                        <img src={image} alt="" />
+                    </div>
+                    <div className="accordion__content___items">
+                        <p>{content}</p>
+                        <div className="accordion__content___buttons">
+                            <AccordionButton
+                                text={'Edit'}
+                                icon={faEdit}
+                                type={'edit'}
+                            />
+                            <AccordionButton
+                                text={'Delete'}
+                                icon={faTrashAlt}
+                                type={'delete'}
+                            />
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
