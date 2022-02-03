c7f1e07216e3e22f1b7482cdb447a0ea3cffe1f3
washington-shoji <shoji.washington@gmail.com>
2022-02-03 23:20:21 +1100

    feat(component): Added developer gallery component

---
+++ src/pages/public/DevelopersGallery.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UsersGallery from '../../components/public-users/components/user-gallery/UsersGallery';
+
+export default function DevelopersGallery() {
+    return (
+        <div>
+            <UsersGallery />
+        </div>
+    );
+}
