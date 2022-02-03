7d1978083f60c2271a7c947a095f9e969016fb98
washington-shoji <shoji.washington@gmail.com>
2022-02-03 23:21:54 +1100

    chore(relocated): Relocated dummy gallery data

---
+++ src/data/dummy-gallery.data.ts
@@ -0,0 +1,39 @@
+export const galleryData = [
+    {
+        id: 1,
+        name: 'Ainara Bernardine',
+        description: `Hello. I am Ainara from Yerevan. Thanks for visiting me.
+        I always respect clients and follow their demands. Contact me so we can discuss the project in detail.`,
+        image: 'https://randomuser.me/api/portraits/women/29.jpg',
+        techStack: 'full-stack',
+        skills: ['Python', 'BlockChain', 'AWS', 'React.js'],
+        rate: 30,
+    },
+    {
+        id: 2,
+        name: 'Levi Theodotus',
+        description: `Having over 8 years of Web and Mobile experience , I'm providing Well Designed and High-quality Websites , Mobile and Web applications. I have extensive experience in a wide range of applications and websites including ecommerce, food delivery, chat messaging, health applications, and corporate apps.`,
+        image: 'https://randomuser.me/api/portraits/men/3.jpg',
+        techStack: 'full-stack',
+        skills: ['React-native', 'Nodejs', 'Django'],
+        rate: 20,
+    },
+    {
+        id: 3,
+        name: 'Sri Cuauhtémoc',
+        description: `I’m Sri, and I’m a Web and App Development Specialist with over 5 years of full stack experience. I’m Serbia-based, but capable of working globally as a top-tier Freelancer. I have developed many websites over the years, working with small businesses, corporate clients and individuals seeking high-end web and app assets.`,
+        image: 'https://randomuser.me/api/portraits/men/74.jpg',
+        techStack: 'front-end',
+        skills: ['React.js', 'Angular', 'Vue.js'],
+        rate: 50,
+    },
+    {
+        id: 4,
+        name: 'Sybil Eugenia',
+        description: `Having 7+ experience in the field of Software Development with vast knowledge of the below-mentioned area of software development. I am dedicated and committed to ensuring quality work is done within the specified time frame.`,
+        image: 'https://randomuser.me/api/portraits/women/15.jpg',
+        techStack: 'back-end',
+        skills: ['Java', 'Kotlin', 'Ionic', 'Android'],
+        rate: 40,
+    },
+];
